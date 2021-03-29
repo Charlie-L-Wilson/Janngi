bc169408040d12863a6ba57f989bbcafac3c81ac
Charlie L. WIlson <leung.chi.hang.eric@gmail.com>
2021-03-28 20:05:50 -0400

    Imported image for the Janggi Board and set up the basic frame for the program.

--- gui.py
+++ gui.py
@@ -1,15 +1,23 @@
 from tkinter import *
+from PIL import ImageTk, Image
 from JanggiGame import *
-
-class Janggi_GUI:
-
-	def __init__(self, master):
-		self.master = master
-		master.title("Janggi")
-		master.geometry("500x500")
-
 
 
 root = Tk()
-play_Janggi = Janggi_GUI(root)
+root.title("Janggi")
+
+status_frame = LabelFrame(root)
+
+status_frame.grid(row=0, column=0)
+
+status_label = Label(status_frame, text="Game Status:")
+status_label.grid(row=0, column=0)
+
+board_frame = LabelFrame(root)
+board_frame.grid(row=1, column=0)
+
+background_image = ImageTk.PhotoImage(Image.open("images/janggi_board.png"))
+background_label = Label(image=background_image)
+background_label.grid(row=1, column=0)
+
 root.mainloop()
